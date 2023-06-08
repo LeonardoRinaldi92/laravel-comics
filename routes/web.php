bc6644ccd0c1f4a8043649d625742490c1f18d60
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-06-08 16:06:17 +0200

    me so scordato di pusahaaareeee

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+
+    $navbar = config('db.navBar');
+    return view('welcome',compact('navbar') );
+})->name('home');
